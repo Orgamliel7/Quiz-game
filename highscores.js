b25b1c9b04e6dda0cd3093b93b117f6c31e02945
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2022-04-07 20:10:26 +0300

    Delete highscores.js

--- highscores.js
+++
@@ -1,9 +0,0 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-
-  highScoresList.innerHTML = highScores
-  .map( (score,index) => {
-    return `<tr><td> ${index + 1} </td> <td>${score.name} </td> <td> ${score.score}</td> </tr>`;
-  })
-  .join("");
